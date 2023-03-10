b4bf5f357713a7a2c2131570f8957ffcdd0189d9
shkiimaa <shkiimaa@gmail.com>
2023-03-10 23:11:28 +0900

    feat: 회원가입 및 북마크 스키마 작성

---
+++ schemas/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    pw: {
+      type: String,
+      required: true,
+    },
+    bookmark: {
+      type: Array,
+      required: false,
+    },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('User', userSchema);
