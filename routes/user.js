1c467f046f1237bcee6507540217796a28bb3643
shkiimaa <shkiimaa@gmail.com>
2023-03-16 21:44:54 +0900

    refactor: 리스폰스 메세지 변경

--- routes/user.js
+++ routes/user.js
@@ -7,16 +7,17 @@ const userModels = require('../models/user');
 
 const router = express.Router();
 
-router.post('/loign', async (req, res) => {
+router.post('/login', async (req, res) => {
   const { id, pw } = req.body;
+  console.log(id, pw);
 
   try {
     const userCheck = await userModels.getUserInfo(id);
 
     if (!userCheck) {
-      return res.status(StatusCodes.NOT_FOUND).json({ errors: { msg: '아이디가 다릅니다.' } });
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: '아이디가 다릅니다.' });
     } else if (!bcrypt.compareSync(pw, userCheck.pw)) {
-      return res.status(StatusCodes.NOT_FOUND).json({ errors: { msg: '비밀번호가 다릅니다.' } });
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: '비밀번호가 다릅니다.' });
     }
 
     const payload = {
@@ -28,7 +29,7 @@ router.post('/loign', async (req, res) => {
     jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' }, (err, token) => {
       if (err) throw err;
 
-      res.json({ token });
+      res.json({ token, msg: '로그인 성공' });
 
       // res
       //   .cookie('user', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
@@ -48,7 +49,7 @@ router.post('/user', async (req, res) => {
     const findId = await userModels.getUserInfo(id);
 
     if (findId) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ errors: { msg: '이미 있는 아이디 입니다.' } });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: '이미 있는 아이디 입니다.' });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -61,9 +62,9 @@ router.post('/user', async (req, res) => {
 
     await User.create(user);
 
-    res.status(StatusCodes.CREATED).json({ success: { msg: '회원가입 성공' } });
+    res.status(StatusCodes.CREATED).json({ msg: '회원가입 성공' });
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: { msg: error.message } });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: error.message });
   }
 });
 
